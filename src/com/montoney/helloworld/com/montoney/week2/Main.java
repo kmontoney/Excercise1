919a5e388045fc78a756e78d6a4ae7432de4800f
kmontoney <kylerdm@columbus.rr.com>
2018-01-23 13:10:59 -0500

    Go away

---
+++ src/com/montoney/helloworld/com/montoney/week2/Main.java
@@ -0,0 +1,19 @@
+package com.montoney.helloworld.com.montoney.week2;
+
+/**
+ * Created by 029089 on 1/23/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Welcome darkness my old friend ");
+        System.out.println(" I have come to speak to you again");
+
+
+
+    }
+
+
+
+
+
+}
